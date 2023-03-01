88ee23409d629bde507cb08ea2b9ea298b707ccc
Кирилл <iorbit4557@gmail.com>
2023-03-01 21:55:15 +0300

    Фронт - шапка

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from .models import Category, OilType, Viscosity, Compound, Fuel, Product
 from cart.forms import CartAddProductForm
+from django.views.generic import ListView
 
 # Create your views here.
 def show_categories(request):
@@ -53,4 +54,17 @@ def show_product_detail(request, product_code:str):
     }
 
     return render(request, 'shop/product_detail.html', data)
+
+class Search(ListView):
+    model = Product
+    template_name = 'shop/search_results.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Product.objects.filter(name_m__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
 
