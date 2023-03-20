4129ba085099f2ce7720117c3e6d2e3a4ca6fff9
Кирилл <iorbit4557@gmail.com>
2023-03-20 22:57:18 +0300

    Правки в странице "О нас"

---
+++ shop/static/shop/js/sliderScript.js
@@ -0,0 +1,24 @@
+// const sliderWrapper = document.querySelector('.slider-wrapper');
+// const prevBtn = document.querySelector('.slider-prev');
+// const nextBtn = document.querySelector('.slider-next');
+// let counter = 0;
+//
+// nextBtn.addEventListener('click', () => {
+//     if (counter < 1){
+//         counter++;
+//         sliderWrapper.style.transform = `translateX(-${counter * 50}%)`;
+//     } else{
+//         counter = 0;
+//         sliderWrapper.style.transform = 'translateX(0%)';
+//     }
+// });
+//
+// prevBtn.addEventListener('click', () => {
+//     if (counter > 0){
+//         counter--;
+//         sliderWrapper.style.transform = `translateX(-${counter * 50}%)`;
+//     } else {
+//         counter = 1;
+//         sliderWrapper.style.transform = `translateX(-${counter * 50}%)`;
+//     }
+// });
