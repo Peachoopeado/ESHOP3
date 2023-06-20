81185b783dfeffd012633a75c88b0b827f91f874
Кирилл <iorbit4557@gmail.com>
2023-06-20 16:52:31 +0300

    Логотип

--- shop/static/shop/js/searchScript.js
+++ shop/static/shop/js/searchScript.js
@@ -6,7 +6,7 @@ const favorite = document.getElementById('fav-button');
 
 searchButton.addEventListener('click', function () {
     searchInput.style.display = 'block';
-    searchButton.style.marginLeft = '130px';
+    searchButton.style.display = 'none'; 
     cart.style.display = 'none';
     dashboard.style.display = 'none';
     favorite.style.display = 'none';
